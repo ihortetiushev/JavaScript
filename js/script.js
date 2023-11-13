32e352be25e3cea6d72daaf2cac317527e967e02
ihortetiushev <ihor.tetiushev@gmail.com>
2023-11-14 00:29:54 +0200

    start commit lb3

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function sighUpDropDownFunction(elementId) {
 }
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
@@ -15,3 +15,23 @@ window.onclick = function(event) {
         }
     }
 }
+let images = ['imgJava.jpg','kobzar.jpg']
+let currentIndex = 0;
+
+function changeNextPic() {
+    currentIndex ++;
+    if(currentIndex>=images.length){
+        currentIndex = 0;
+    }
+    let image = images[currentIndex]
+    document.getElementById("qqq").src = 'images/' + image
+}
+function changePreviousPic() {
+    currentIndex --;
+    if(currentIndex < 0){
+        currentIndex = images.length -1;
+    }
+    let image = images[currentIndex]
+    document.getElementById("qqq").src = 'images/' + image
+}
+
